4af8eeabf534240ed7fd3a20dc882bd078b25c49
Omar Zenith Lazares <OmarZenithLazares@gmail.com>
2022-01-25 20:31:07 -0600

    [ADD] Added the product interface

---
+++ back_end/back_end/Repositories/Interfaces/IProductInterface.cs
@@ -0,0 +1,22 @@
+﻿using back_end.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back_end.Repositories.Interfaces
+{
+    interface IProductInterface
+    {
+        Task<int> Create(ProductModel product_Model);
+
+
+        Task<List<ProductModel>> Read();
+
+
+        Task<int> Update(ProductModel product_Model);
+
+
+        Task<int> Delete(int product_ID);
+    }
+}
