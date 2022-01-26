1a56cd247391414f23b311ab34c6ad20aa300e6a
Omar Zenith Lazares <OmarZenithLazares@gmail.com>
2022-01-25 20:38:27 -0600

    [ADD] Added the order interface

---
+++ back_end/back_end/Repositories/Interfaces/IOrderInterface.cs
@@ -0,0 +1,22 @@
+﻿using back_end.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back_end.Repositories.Interfaces
+{
+    public interface IOrderInterface
+    {
+        Task<int> Create(OrderModel order_Model);
+
+
+        Task<List<OrderModel>> Read();
+
+
+        Task<int> Update(OrderModel order_Model);
+
+
+        Task<int> Delete(int order_ID);
+    }
+}
