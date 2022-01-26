01e2ecc9d1959964185a92814797cffe513484b5
Omar Zenith Lazares <OmarZenithLazares@gmail.com>
2022-01-25 21:14:14 -0600

    [ADD] Added the product view model

---
+++ back_end/back_end/ViewModels/ProductViewModel.cs
@@ -0,0 +1,62 @@
+﻿using back_end.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace back_end.ViewModels
+{
+    public class ProductViewModel
+    {
+        /// <summary> 
+        /// Used to support REST create operations on the multimedia view. 
+        /// </summary>
+        public class CreateProductViewModel : ProductModel
+        {
+            [Required]
+            public override string Product_SKU { get; set; }
+
+            [Required]
+            public override string Product_Name { get; set; }
+
+            [Required]
+            [Range(0, int.MaxValue)]
+            public override int Product_Quantity { get; set; }
+        }
+
+
+
+        /// <summary> 
+        /// Used to support REST update operations on the multimedia view. 
+        /// </summary>
+        public class UpdateProductViewModel : ProductModel
+        {
+            [Required]
+            [Range(0, int.MaxValue)]
+            public override int Product_ID { get; set; }
+
+            [Required]
+            public override string Product_SKU { get; set; }
+
+            [Required]
+            public override string Product_Name { get; set; }
+
+            [Required]
+            [Range(0, int.MaxValue)]
+            public override int Product_Quantity { get; set; }
+        }
+
+
+
+        /// <summary> 
+        /// Used to support REST delete operations on the multimedia view. 
+        /// </summary>
+        public class DeleteProductViewModel : ProductModel
+        {
+            [Required]
+            [Range(0, int.MaxValue)]
+            public override int Product_ID { get; set; }
+        }
+    }
+}
